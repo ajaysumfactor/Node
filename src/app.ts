06bd8a01f05a452fb517279bbd5e4209378451d2
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-21 17:22:07 +0530

    url module

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
-// import {createServer,IncomingMessage,ServerResponse} from 'http';
+import * as url from "url";
+import * as path from 'path';
+import * as fs from 'fs';
+
+
+import {createServer,IncomingMessage,ServerResponse} from 'http';
  
-// const PORT: Number =Number(process.env.PORT || '3000');
+const PORT: Number =Number(process.env.PORT || '3000');
 
 // console.log(PORT);
  
@@ -27,40 +32,67 @@
 
 
 // path--module-->to handle path of system
-import * as path from 'path'
-// console.log({path});
-console.log(__dirname);//print current directory in which currently am i.(E:\INTERNSHIP\NODE\express-ts\src)
-const dirPath=__dirname;
-console.log(__filename);//print the name of the file in which i have open(E:\INTERNSHIP\NODE\express-ts\src\app.ts)
-let newfilePath=path.join(dirPath,"text.js");//E:\INTERNSHIP\NODE\express-ts\src\text.js
-console.log(newfilePath);
-let extName=path.extname("E:\INTERNSHIP\NODE\express-ts\src\text.js");
-console.log(extName);//.js
-let baseName=path.basename("E:/INTERNSHIP/NODE/express-ts/src/text.js");
-console.log(baseName);//text.js
+// // console.log({path});
+// console.log(__dirname);//print current directory in which currently am i.(E:\INTERNSHIP\NODE\express-ts\src)
+// const dirPath=__dirname;
+// console.log(__filename);//print the name of the file in which i have open(E:\INTERNSHIP\NODE\express-ts\src\app.ts)
+// let newfilePath=path.join(dirPath,"text.js");//E:\INTERNSHIP\NODE\express-ts\src\text.js
+// console.log(newfilePath);
+// let extName=path.extname("E:\INTERNSHIP\NODE\express-ts\src\text.js");
+// console.log(extName);//.js
+// let baseName=path.basename("E:/INTERNSHIP/NODE/express-ts/src/text.js");
+// console.log(baseName);//text.js
 
 
-// fs module--> to handle file system
-import * as fs from 'fs';
-// write to a file
-let filePath=path.join(__dirname,"file.txt");
-console.log(filePath);
-//C-create
-//it creates file if it does not exists else it override
-fs.writeFileSync(filePath,"Hello I am a text file ");
+// // fs module--> to handle file system
+// import * as fs from 'fs';
+// // write to a file
+// let filePath=path.join(__dirname,"file.txt");
+// console.log(filePath);
+// //C-create
+// //it creates file if it does not exists else it override
+// fs.writeFileSync(filePath,"Hello I am a text file ");
 
-// read file
-const readFile = fs.readFileSync(filePath,'utf-8' );
-console.log(readFile);
+// // read file
+// const readFile = fs.readFileSync(filePath,'utf-8' );
+// console.log(readFile);
 
-// //U-update
-fs.appendFileSync(filePath,"\nNewly added content");
-console.log("After update : ")
-console.log(fs.readFileSync(filePath,'utf-8'));
+// // //U-update
+// fs.appendFileSync(filePath,"\nNewly added content");
+// console.log("After update : ")
+// console.log(fs.readFileSync(filePath,'utf-8'));
 
-//delete
-//delete
-fs.unlinkSync(filePath);
+// //delete
+// //delete
+// fs.unlinkSync(filePath);
+
+
+
+
+ 
+ 
+  
+const server=createServer((request:IncomingMessage,response: ServerResponse)=>{
+  console.log({request});
+     if(request.url=="/address"){
+        if(request.method==="GET"){
+
+          var adr = 'http://localhost:8080/default.htm?year=2017&month=february&weather=summer';
+        
+          var q = url.parse(adr, true);
+  
+          console.log("data", q)
+          response.end("success");
+        }else{
+            response.end("Worng method for this api")
+        }
+    }
+    else{
+      response.end("Please enter valid route");
+    }
+});
+server.listen(PORT, () => console.log(`server islistining at port ${PORT}`))
+
 
 
 
