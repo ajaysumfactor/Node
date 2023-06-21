ea24266e2c03eabc996d2199af69af1a75e86689
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-21 11:41:18 +0530

    made http server and use get to send request

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
+import {createServer,IncomingMessage,ServerResponse} from 'http';
  
+const PORT: Number =Number(process.env.PORT || '3000');
+
+console.log(PORT);
+ 
+const server=createServer((request:IncomingMessage,response: ServerResponse)=>{
+  console.log({request});
+     if(request.url=="/name"){
+        if(request.method==="GET"){
+
+            response.end("hello world")
+        }else{
+            response.end("Worng method for this api")
+        }
+    }
+});
+server.listen(PORT, () => console.log(`server islistining at port ${PORT}`))
