c51eebaedf02ecceda48d6185da632b3dccc46fe
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-21 12:29:23 +0530

    implement path module

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,36 @@
-import {createServer,IncomingMessage,ServerResponse} from 'http';
+// import {createServer,IncomingMessage,ServerResponse} from 'http';
  
-const PORT: Number =Number(process.env.PORT || '3000');
+// const PORT: Number =Number(process.env.PORT || '3000');
 
-console.log(PORT);
+// console.log(PORT);
  
-const server=createServer((request:IncomingMessage,response: ServerResponse)=>{
-  console.log({request});
-     if(request.url=="/name"){
-        if(request.method==="GET"){
+// const server=createServer((request:IncomingMessage,response: ServerResponse)=>{
+//   console.log({request});
+//      if(request.url=="/name"){
+//         if(request.method==="GET"){
 
-            response.end("hello world")
-        }else{
-            response.end("Worng method for this api")
-        }
-    }
-});
-server.listen(PORT, () => console.log(`server islistining at port ${PORT}`))
+//             response.end("hello world")
+//         }else{
+//             response.end("Worng method for this api")
+//         }
+//     }
+// });
+// server.listen(PORT, () => console.log(`server islistining at port ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+// fs--module-->to handle the file system
+import * as path from 'path'
+// console.log({path});
+console.log(__dirname);//print current directory in which currently am i.(E:\INTERNSHIP\NODE\express-ts\src)
+const dirPath=__dirname;
+console.log(__filename);//print the name of the file in which i have open(E:\INTERNSHIP\NODE\express-ts\src\app.ts)
+let newfilePath=path.join(dirPath,"text.js");//E:\INTERNSHIP\NODE\express-ts\src\text.js
+console.log(newfilePath);
